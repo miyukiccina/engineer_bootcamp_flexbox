cadb01c47188082fc2f461d6af6522b8b52cefd0
miyukiccina <80190927+miyukiccina@users.noreply.github.com>
2021-03-17 19:58:11 +0900

    first commit
    講義の記録

---
+++ kadai/test.js
@@ -0,0 +1,17 @@
+// alert(11111111)
+console.log("はじめてのジャバスクリプト")
+console.log(23+5);
+console.log(2000-1800);
+console.log("18+5");
+
+// 演習３
+var name="西田未由紀"
+console.log(name);
+// ↓これが判断の基準となる
+var point="90"
+
+if(point>89){
+    console.log("素晴らしい！おめでとう！")
+}else{
+        console.log("もっと頑張ろう！")
+    }
